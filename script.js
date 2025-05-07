48fc7f98604f17dcf79c0090cbf25db1c1b12f6f
akshit <gargakshit190@gmail.com>
2025-05-07 14:34:55 +0530

    update styling

---
+++ script.js
@@ -0,0 +1,14 @@
+// let change=document.querySelector("#select").value;
+// change.addEventListener("change",changecity);
+
+function changecity() {
+    let change = document.querySelector("#new").value;
+    window.location.href = "newdelhi.html";
+    console.log(change);
+}
+// window.location.href = "newdelhi.html";
+function change() {
+    let change = document.querySelector("#go").value;
+    window.location.href = "index.html";
+    console.log(change);
+}
